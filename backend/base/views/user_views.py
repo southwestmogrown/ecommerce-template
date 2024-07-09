1769373e594b28e5ba00574e31ca9cc0577bc223
southwestmogrown <southwestmogrown@gmail.com>
2024-07-09 18:32:04 -0500

    cleanup views and urls

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -2,9 +2,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
+from base.models import Product
 from django.contrib.auth.models import User
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -58,18 +58,3 @@ def getUsers(request):
   users = User.objects.all()
   serializer = UserSerializer(users, many=True)
   return Response(serializer.data)
-
-@api_view(['GET'])
-def getProducts(request):
-  products = Product.objects.all()
-  serializer = ProductSerializer(products, many=True)
-  return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request, id):
-  product = Product.objects.get(_id = id)
-  serializer = ProductSerializer(product, many=False)
-
-  return Response(serializer.data)
-
